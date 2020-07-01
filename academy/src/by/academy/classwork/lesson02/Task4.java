054b7ac3650dbf59c6f524b27e53b7a71b3e7cfb
AlexanderK <sashakuharchyk@gmail.com>
2020-07-01 16:02:59 +0300

    new

--- academy/src/by/academy/classwork/lesson02/Task4.java
+++ academy/src/by/academy/classwork/lesson02/Task4.java
@@ -4,11 +4,11 @@ public class Task4 {
 	public static void main(String[] args) {
 		int a = 3;
 		if(a%2==0) {
-			System.out.println("Chetnoe");
+			System.out.println("Четное");
 		}
 		else
 		{
-			System.out.println("Nechetnoe");
+			System.out.println("Нечетное");
 		}
 		
 	}
