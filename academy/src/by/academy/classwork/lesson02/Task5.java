15c14b663b0313f55adb775aac46c314f2f9ceef
AlexanderK <sashakuharchyk@gmail.com>
2020-07-01 15:43:45 +0300

    new

--- academy/src/by/academy/classwork/lesson02/Task5.java
+++ academy/src/by/academy/classwork/lesson02/Task5.java
@@ -2,18 +2,21 @@ package by.academy.classwork.lesson02;
 
 public class Task5 {
 	public static void main(String[] args) {
-		int s = 3700;
+		int s = 695800;
 		int sec;
 		int m;
-		int min,h;
-		int day, week;
+		int min,h,hour;
+		int d,day,w, week;
 		sec = s % 60;
 		m = (s - sec) / 60;
 		min = m % 60;
 		h = (m - min) / 60;
-		day = h % 24;
-		week = day % 7;
-		System.out.println(h + "hours" + min + "min" + sec + "seconds");
+		hour = h % 24;
+		d = (h - hour) / 24;
+		day = d % 7;
+		w = (d - day) / 7;
+		week = w % 7;
+		System.out.println(week + "weeks" + day + "days" + hour + "hours" + min + "min" + sec + "seconds");
 		
 	}
 }
