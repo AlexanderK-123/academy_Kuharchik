3fdbb40d200ac413555d92c858af8e41e4148659
Alexandr Kuharchik <sashakuharchyk@gmail.com>
2020-08-24 20:07:10 +0300

    lesson 15 new

--- academy/src/by/academy/classwork/lesson14/task2/FileInputOutputStreamDemo.java
+++ academy/src/by/academy/classwork/lesson14/task2/FileInputOutputStreamDemo.java
@@ -1,7 +1,6 @@
 package by.academy.classwork.lesson14.task2;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
