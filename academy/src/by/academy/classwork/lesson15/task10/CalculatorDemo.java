47a5121161849a5b9ad59e0c4ada45d4d51fb856
Alexandr Kuharchik <sashakuharchyk@gmail.com>
2020-08-26 21:10:59 +0300

    lesson 16

---
+++ academy/src/by/academy/classwork/lesson15/task10/CalculatorDemo.java
@@ -0,0 +1,24 @@
+package by.academy.classwork.lesson15.task10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		Map<String, CalculatorFunction> catalog = new HashMap<>();
+
+		catalog.put("+", (a, b) -> a + b);
+		catalog.put("-", (a, b) -> a - b);
+		catalog.put("*", (a, b) -> a * b);
+		catalog.put("/", (a, b) -> a / b);
+
+		double a = 5, b = 9;
+
+		System.out.println(catalog.get("+").calc(a, b));
+		System.out.println(catalog.get("-").calc(a, b));
+		System.out.println(catalog.get("*").calc(a, b));
+		System.out.println(catalog.get("/").calc(a, b));
+	}
+
+}
---
+++ academy/src/by/academy/classwork/lesson15/task10/CalculatorFunction.java
@@ -0,0 +1,6 @@
+package by.academy.classwork.lesson15.task10;
+
+@FunctionalInterface
+public interface CalculatorFunction {
+	double calc(double a, double b);
+}
