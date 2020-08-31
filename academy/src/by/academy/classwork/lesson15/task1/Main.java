5c02404763f09565b07afe3f6f861417741e2916
AlexanderK <sashakuharchyk@gmail.com>
2020-08-31 12:00:26 +0300

    new_update

--- academy/src/by/academy/classwork/lesson15/task1/Main.java
+++ academy/src/by/academy/classwork/lesson15/task1/Main.java
@@ -3,12 +3,9 @@ package by.academy.classwork.lesson15.task1;
 public class Main implements Printable {
 
 	public static void main(String[] args) {
-		String s = "string";
-		//Printable p = s -> System.out.println(s);
-		
-		Main m = new Main();
-		
-		m.print(s);
+		Printable p = s -> System.out.println(s);
+		String s = "String";
+		p.print(s);
 	}
 
 	@Override
